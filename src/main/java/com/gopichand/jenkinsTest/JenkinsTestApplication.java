54e200b6fd0cd0ff86410abab201008eca754e2a
GopichandPaidipally <paidipallygopi95@gmail.com>
2023-04-14 17:24:51 +0530

    class chnage

--- src/main/java/com/gopichand/jenkinsTest/JenkinsTestApplication.java
+++ src/main/java/com/gopichand/jenkinsTest/JenkinsTestApplication.java
@@ -15,7 +15,7 @@ public class JenkinsTestApplication {
 	@PostConstruct
 	public void init() {
 		
-		logger.info("Application Started ..........");
+		logger.info("Application Started now..........");
 		
 	}
 
